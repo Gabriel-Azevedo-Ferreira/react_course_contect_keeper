0e31ef96281fb98c3ebee74cb9d1398cdd0cb7f3
GabrielAz <gabriel.azevedoferreira@quantumblack.com>
2021-09-03 20:43:19 +0800

    checks on users post schema

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const {check, validationResult} = require('express-validator/check')
+
+const Users = require('../models/User')
+
 
 // @route   POST api/users
 // @desc    Register a user
 // @access  Public
-router.post('/', (req, res) => {
-    res.send("register a user");
-})
+router.post(
+    '/',
+    [
+        check('name', 'Name required...').not().isEmpty(),
+        check('email', 'Valid email required...').isEmail(),
+        check('password', 'PW >6 charcaer...').isLength({min: 6}),
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) {
+            return res.status(400).json({errors: errors.array()})
+        }
+        res.send(req.body);
+    })
 
 module.exports = router;
