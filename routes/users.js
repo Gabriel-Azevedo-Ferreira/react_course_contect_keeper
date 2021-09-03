f6a6f4f08d2df0dbe20a296a396a9f0031c384dc
GabrielAz <gabriel.azevedoferreira@quantumblack.com>
2021-09-03 21:56:12 +0800

    adding request for saving user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 const {check, validationResult} = require('express-validator/check')
 
-const Users = require('../models/User')
+const User = require('../models/User')
 
+// todo: why the check does not use the schema
 
 // @route   POST api/users
 // @desc    Register a user
@@ -15,11 +17,27 @@ router.post(
         check('email', 'Valid email required...').isEmail(),
         check('password', 'PW >6 charcaer...').isLength({min: 6}),
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) {
             return res.status(400).json({errors: errors.array()})
+        }
+        const {name, email, password} = req.body
+
+        try {
+            let user = await User.findOne({email})
+            if (user) {
+                return res.status(400).json({msg: 'user exists'})
+            }
+            user = new User({name, email, password})
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            await user.save()
+            res.send('User send')
+        } catch (err) {
+            console.log(err.message)
+            return res.status(500).send('server error')
         }
         res.send(req.body);
     })
